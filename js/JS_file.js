42b70f417b9470ca60b457008863fc387ca1fbcf
IdelIshbaev <41520293+IdelIshbaev@users.noreply.github.com>
2021-01-24 18:30:40 +0300

    menu animation
    mobile menu animation
    fade in and fade out

--- js/JS_file.js
+++ js/JS_file.js
@@ -1,12 +1,23 @@
+
 function hamburgerTransform(){
 	btn = document.getElementById("hamburgerBTN");
 	overlay = document.getElementById("overlay");
 	body = document.body;
 
-	
+	// mobile menu
 	btn.classList.toggle('open');
 	overlay.classList.toggle('show-mobile-menu')
 	body.classList.toggle('overflow-hidden')
 
+	// fade in and fade out for mobile menu
+	if( overlay.classList.contains('show-mobile-menu')){
+		overlay.classList.add('fade-in')
+		overlay.classList.remove('fade-out')
+	} else{
+		overlay.classList.remove('fade-in')
+		overlay.classList.add('fade-out')
+	}
+
 
 }
+
